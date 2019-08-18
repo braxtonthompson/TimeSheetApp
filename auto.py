b43eaac2a11f987b3a79bff4a04a268eec36ae59
braxtontthompson <i3raxton@gmail.com>
2019-08-17 21:05:50 -0500

    Added .getignore

--- auto.py
+++ auto.py
@@ -1,4 +1,19 @@
 from selenium import webdriver
+import time
+import credentials
 
 driver = webdriver.Chrome()
-driver.get('http://www.google.com')
+
+def auto():
+    driver.get('https://ulink.louisiana.edu')
+    driver.find_element_by_id('username').send_keys(credentials.USERNAME)
+    driver.find_element_by_id('password').send_keys(credentials.PASSWORD)
+    driver.find_element_by_xpath('//*[@id="loginForm"]/div[4]/input').click()
+    driver.find_element_by_class_name('body').send_keys(ESCAPE)
+    driver.find_element_by_xpath('//*[@id="layout_33"]/a').click()
+    
+    time.sleep(15)
+    driver.close()
+
+#---main---
+auto()
