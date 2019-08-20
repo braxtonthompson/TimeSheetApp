3ffea28d6570411200c6ef235db6d661b91e1c65
braxtontthompson <i3raxton@gmail.com>
2019-08-20 03:22:09 -0500

    It's all about the PAGE LOAD STRAT

--- main.py
+++ main.py
@@ -6,20 +6,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-driver = webdriver.Chrome()
 
 def auto():
+    caps = DesiredCapabilities().CHROME
+    caps["pageLoadStrategy"] = "none"
+    driver = webdriver.Chrome(desired_capabilities=caps)
+    # driver = webdriver.Chrome()
+    driver.set_page_load_timeout(10)
+
+    
     driver.get('https://ulink.louisiana.edu')
+    time.sleep(5)
     driver.find_element_by_id('username').send_keys(credentials.USERNAME)
     driver.find_element_by_id('password').send_keys(credentials.PASSWORD)
-
-    driver.find_element_by_xpath('//*[@id="loginForm"]/div[4]/input').click()
-    driver.find_element_by_tag_name('body').send_keys("Keys.ESCAPE")
-
-    driver.find_element_by_class_name('item6').click()
-    
     time.sleep(5)
+    element = driver.find_element_by_xpath('//*[@id="loginForm"]/div[4]/input')
+    time.sleep(5)
+    element.click()
+
+
+    time.sleep(10)
     driver.quit()
 
 #---main---
