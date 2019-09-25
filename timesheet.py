7917efdccb04f14f8e45d4ddc1b61fc0ff8b73d7
braxtontthompson <i3raxton@gmail.com>
2019-09-24 19:14:52 -0500

    Basic page setup.

--- timesheet.py
+++ timesheet.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-        return "<h1>Hello World!<h1>"
+@app.route("/home")
+def home():
+        return render_template('home.html')
+
+@app.route("/timesheet")
+def timesheet():
+        return render_template('timesheet.html')
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
