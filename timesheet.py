d3ff93b5bdf917b8c9bb9876a88ea01ffbc56a2b
braxtontthompson <i3raxton@gmail.com>
2019-09-24 19:37:06 -0500

    Entered html + css.

--- timesheet.py
+++ timesheet.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route("/")
-@app.route("/home")
-def home():
-        return render_template('home.html')
-
 @app.route("/timesheet")
 def timesheet():
         return render_template('timesheet.html')
