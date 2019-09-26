6d9e2d499ae36b413796fac6060ddde71d4b2453
braxtontthompson <i3raxton@gmail.com>
2019-09-26 18:37:25 -0500

    Implemented form.

--- timesheet.py
+++ timesheet.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, url_for
+import credentials
+from flask import Flask, render_template, url_for, redirect
+from forms import TimesheetForm
+
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/timesheet")
+app.config['SECRET_KEY'] = credentials.SECRET_KEY
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/timesheet", methods=['GET', 'POST'])
 def timesheet():
-        return render_template('timesheet.html')
+        form = TimesheetForm()
+        if form.validate_on_submit():
+                return redirect(url_for('timesheetprogress'))
+        return render_template('timesheet.html', title='Trouble Crew Time Sheets', form=form)
+
+@app.route("/timesheetprogress")
+def timesheetprogress():
+        return render_template('timesheetprogress.html', title='Trouble Crew Time Sheets')
 
 
 if __name__ == '__main__':
