a1b02335930adac6de41cae9ec2d78de6b814fd5
braxtontthompson <i3raxton@gmail.com>
2019-09-26 16:31:40 -0500

    Recreated virtual environment.

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/braxton/Projects/Coding/MyProjects/TimeSheetApp/venv"
+set -gx VIRTUAL_ENV "/Users/braxton/Projects/TimeSheetApp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
