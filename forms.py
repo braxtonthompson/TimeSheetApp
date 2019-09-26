e4d879bc1cfbec9f6cedb061ef2d063d94e1a031
braxtontthompson <i3raxton@gmail.com>
2019-09-26 18:36:43 -0500

    Created Timesheet form.

--- forms.py
+++ forms.py
@@ -1,1 +1,10 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired, Length, NumberRange
+
+class TimesheetForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=9, max=9, message='Invalid C-Number.')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    hours_worked = IntegerField('Hours Worked', validators=[DataRequired(), NumberRange(min=30, max=100, message='Please enter a valid number of hours.')])
+    submit = SubmitField('Submit')
+    remember = BooleanField('Remember Info')
