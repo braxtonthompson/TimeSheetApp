b8dcc53fec9374caad20ce55aea10854a2e70f38
braxtontthompson <i3raxton@gmail.com>
2019-09-26 19:22:15 -0500

    Setup a dummy method upon forum POST.

--- timesheet.py
+++ timesheet.py
@@ -4,13 +4,17 @@ from forms import TimesheetForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = credentials.SECRET_KEY
+app.config['SECRET_KEY'] = 'c325bf048d4444685df73bfa95ce454c'
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/timesheet", methods=['GET', 'POST'])
 def timesheet():
         form = TimesheetForm()
         if form.validate_on_submit():
+                USERNAME = form.username.data
+                PASSWORD = form.password.data
+                HOURS = form.hours_worked.data
+                print (f"{USERNAME}\n{PASSWORD}\n{HOURS}")
                 return redirect(url_for('timesheetprogress'))
         return render_template('timesheet.html', title='Trouble Crew Time Sheets', form=form)
 
