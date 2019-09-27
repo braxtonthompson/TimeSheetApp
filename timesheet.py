e2cf5cf54c5b4256bc4df4f4e2296860ad2f1418
braxtontthompson <i3raxton@gmail.com>
2019-09-27 14:25:16 -0500

    Send form data to script.py

--- timesheet.py
+++ timesheet.py
@@ -1,3 +1,4 @@
+import script
 import credentials
 from flask import Flask, render_template, url_for, redirect
 from forms import TimesheetForm
@@ -11,17 +12,18 @@ app.config['SECRET_KEY'] = 'c325bf048d4444685df73bfa95ce454c'
 def timesheet():
         form = TimesheetForm()
         if form.validate_on_submit():
-                USERNAME = form.username.data
-                PASSWORD = form.password.data
-                HOURS = form.hours_worked.data
-                print (f"{USERNAME}\n{PASSWORD}\n{HOURS}")
+                credentials.USERNAME = form.username.data
+                credentials.PASSWORD = form.password.data
+                credentials.HOURS = form.hours_worked.data
+                print (f'Username: {credentials.USERNAME}\nPassword: {credentials.PASSWORD}\nHours worked: {credentials.HOURS}\nInfo Recieved!')
+                script.segments()
+                script.selenium_script()
                 return redirect(url_for('timesheetprogress'))
         return render_template('timesheet.html', title='Trouble Crew Time Sheets', form=form)
 
 @app.route("/timesheetprogress")
 def timesheetprogress():
         return render_template('timesheetprogress.html', title='Trouble Crew Time Sheets')
-
 
 if __name__ == '__main__':
         app.run(debug=True)
