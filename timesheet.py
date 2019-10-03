0fe74d050be16d82465bd7275416b38a6f17c432
braxtontthompson <i3raxton@gmail.com>
2019-10-03 17:16:38 -0500

    Changed form completion route name.

--- timesheet.py
+++ timesheet.py
@@ -8,21 +8,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = credentials.SECRET_KEY
 
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/timesheet", methods=['GET', 'POST'])
 def timesheet():
         form = TimesheetForm()
         if form.validate_on_submit():
+                credentials.NAME = form.name.data
                 credentials.USERNAME = form.username.data
                 credentials.PASSWORD = form.password.data
                 credentials.HOURS = form.hours_worked.data
                 script.segments()
                 script.selenium_script()
-                return redirect(url_for('timesheetstatus'))
+                return redirect(url_for('timesheetcomplete'))
         return render_template('timesheet.html', title='Trouble Crew Time Sheets', form=form)
 
 @app.route("/timesheet/completion")
-def timesheetstatus():
-        return render_template('timesheetprogress.html', title='Trouble Crew Time Sheets')
+def timesheetcomplete():
+        return render_template('timesheetinprogress.html', title='Trouble Crew Time Sheets')
 
 if __name__ == '__main__':
         app.run(debug=True)
