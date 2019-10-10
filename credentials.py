8a9df313f5643866a1f21d91e5f08ca39e07332e
braxtontthompson <i3raxton@gmail.com>
2019-10-10 18:44:16 -0500

    Heroku testing.

---
+++ credentials.py
@@ -0,0 +1,17 @@
+SECRET_KEY = 'c325bf048d4444685df73bfa95ce454c'
+
+RECIPIENT = 'i3raxton@gmail.com'
+
+# Luke's Email
+# C00101509@louisiana.edu
+
+# Dana's Email
+# dana.jones@louisiana.edu
+
+# Braxton's Email
+# C00412627@louisiana.edu
+
+# Jacques' Info
+# C00078076@louisiana.edu
+# C00078076
+# Jpdub1597
