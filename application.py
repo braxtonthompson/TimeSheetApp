1cf6363d381913bb3da5b17d03f7d8eaea40a5f6
braxtontthompson <i3raxton@gmail.com>
2019-10-10 15:28:48 -0500

    Renamed timesheet.py to work with Elastic Beanstalk.

---
+++ application.py
@@ -0,0 +1,24 @@
+import script
+import credentials
+from flask import Flask, render_template, url_for, redirect
+from forms import TimesheetForm
+
+application = app = Flask(__name__)
+
+app.config['SECRET_KEY'] = credentials.SECRET_KEY
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/timesheet", methods=['GET', 'POST'])
+def timesheet():
+        form = TimesheetForm()
+        if form.validate_on_submit():
+                script.Automation(form.username.data, form.password.data, form.hours_worked.data)
+                return redirect(url_for('timesheetcomplete'))
+        return render_template('timesheet.html', form=form)
+
+@app.route("/timesheet/complete")
+def timesheetcomplete():
+        return render_template('timesheet_complete.html')
+
+if __name__ == '__main__':
+        app.run(debug=True)
