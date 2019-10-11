67de1febff7ebab4b1cfcd041c3fe71278749e9e
braxtontthompson <i3raxton@gmail.com>
2019-10-10 20:36:51 -0500

    Changed some routes.

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ application = app = Flask(__name__)
 app.config['SECRET_KEY'] = credentials.SECRET_KEY
 
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/timesheet", methods=['GET', 'POST'])
 def timesheet():
         form = TimesheetForm()
         if form.validate_on_submit():
@@ -16,9 +15,9 @@ def timesheet():
                 return redirect(url_for('timesheetcomplete'))
         return render_template('timesheet.html', form=form)
 
-@app.route("/timesheet/complete")
+@app.route("/complete")
 def timesheetcomplete():
         return render_template('timesheet_complete.html')
 
-# if __name__ == '__main__':
-#         application.run(host='0.0.0.0')
+if __name__ == '__main__':
+        application.run(host='0.0.0.0')
