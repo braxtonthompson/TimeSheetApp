bc91524335b4d9ed9da96bad6c5242295ad3bfd4
braxtontthompson <i3raxton@gmail.com>
2019-10-17 01:10:32 -0500

    Script 3.0

--- script.py
+++ script.py
@@ -1,137 +1,114 @@
-import credentials
-
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from config import config
+import time, math, smtplib, ssl
 
-import smtplib, ssl
+class Auto:
 
-import time
-import math
-
-
-class Automation:
-    
-    def __init__(self, username, password, hours):
-        self.name = ''
+    def __init__(self, username, password, hours_worked):
+        self.name = None
         self.username = username
         self.password = password
-        self.hours = 0
-        self.number_of_segments, self.segment_hours = Automation.segments(self)
-        self.time_period = ''
+        self.hours_worked = hours_worked
+        self.number_of_segments = None
+        self.segment_hours = None
+        self.timesheet_period = None
 
-        Automation.selenium_script(self)
+        Auto.segments(self)
+        Auto.script(self)
 
     def segments(self):
         """Takes the user submitted "hours worked" and divides it into chunks of 20 with a remainder.
         Returns the chunks in a list."""
 
-        self.number_of_segments = math.ceil(self.hours / 20)
+        self.number_of_segments = math.ceil(self.hours_worked / 20)
         self.segment_hours = []
 
         x = 0
         for i in range(self.number_of_segments):
             i # Removes 'problem' from IDE.
             x += 20
-            if x <= self.hours:
+            if x <= self.hours_worked:
                 self.segment_hours.append(20)
             else:
-                self.segment_hours.append(self.hours % 20)
+                self.segment_hours.append(self.hours_worked % 20)
 
-        return self.number_of_segments, self.segment_hours
+        print(self.number_of_segments, self.segment_hours)
 
-    def selenium_script(self):
-        """This is the main script that runs the headless Chromedriver with Selenium. 
-        It navigates through ULink using user given credentials. It travels to banner and opens up the user's timesheet.
-        Then, it invokes segments() break up the user inputted "hours worked" into segments of 20 hours.
-        It inputs the chunks of 20 and then submits the timesheet. Lastly, it sends an email to our manager and the sender."""
-
-        start_time = time.time()
-
-        # Browser Config
+    def script(self):
+        """Opens Banner in a chrome browser and submits hours worked into the timesheet."""
         options = webdriver.ChromeOptions()
-        # options.add_argument('headless')
-        options.add_argument('disable-gpu')
-        options.add_argument('no-sandbox')
-        options.add_experimental_option( "prefs",{'profile.managed_default_content_settings.javascript': 2})
-        capabilities = DesiredCapabilities().CHROME
-        capabilities["pageLoadStrategy"] = "none"
-        driver = webdriver.Chrome(desired_capabilities=capabilities, options=options)
-        driver.set_window_size(1920, 1080)
-        driver.set_page_load_timeout(20)
-        wait = WebDriverWait(driver, 20)
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
 
-        # Open Chrome
-        driver.get('https://ulink.louisiana.edu')
+        # Open Banner
+        driver.get('https://ssb-prod.ec.louisiana.edu/ssomanager/c/SSB?pkg=bwpktais.P_SelectTimeSheetRoll')
 
-        # ULink Login
-        wait.until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(self.username)
-        wait.until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(self.password)
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginForm"]/div[4]/input'))).click()
+        # Log In To Banner
+        driver.find_element_by_id('username').send_keys(self.username)
+        driver.find_element_by_id('password').send_keys(self.password)
+        driver.find_element_by_xpath('//input[@type="submit"]').click()
 
-        # Travel to Banner
-        self.name = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/nav[2]/div[3]/div/div/div/div[1]/div/div/span[2]/div[2]/ul/li[3]/a/span/span'))).text
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="layout_33"]/a/span'))).click()
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="portlet_56_INSTANCE_ZJ9sUpbDoQCa"]/div/div/div/div[1]/p/a'))).click()
-        driver.switch_to.window(driver.window_handles[1])
+        # Open Timesheet
+        driver.find_element_by_id('id____UID5').click()
 
-        # Banner - Open Time Sheet
-        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="id____UID5"]/div/div/div'))).click()
+        # Get name
+        full_name = driver.execute_script('return userDetails;')
+        short_name = full_name.split()
+        del short_name[1]
+        self.name = ' '.join(short_name)
+        print(self.name)
 
-        # Banner - Collect Status
-        banner_status = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[5]/td/form/table[1]/tbody/tr[2]/td[4]/p'))).text
+        # Enter Hours If Not Already Submitted
+        banner_status = driver.find_element_by_xpath('//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[5]/td/form/table[1]/tbody/tr[2]/td[4]/p').text
 
-        # Banner - Collect Data
-        self.time_period = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[3]/td'))).text
+        # Collect Timesheet Period
+        timesheet_period = driver.find_element_by_xpath('//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[3]/td').text
 
-        if int(banner_status) != 0:
-            print('Submitted!')
-        else:
-            # Banner - Enter Time Sheet Hours
-            y = 7
+        if (int(banner_status) == 0):
             for i in range(self.number_of_segments):
                 if i == 0:
-                    wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[5]/td/form/table[1]/tbody/tr[2]/td[' + str(y) + ']/p/a'))).click()
+                    driver.find_element_by_xpath(f'//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[5]/td/form/table[1]/tbody/tr[2]/td[{i + 7}]/p/a').click()
                 else:
-                    wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[6]/td/form/table[1]/tbody/tr[2]/td[' + str(y) + ']/p/a'))).click()
+                    driver.find_element_by_xpath(f'//*[@id="contentHolder"]/div[2]/table[1]/tbody/tr[6]/td/form/table[1]/tbody/tr[2]/td[{i + 7}]/p/a').click()
+                
+                driver.find_element_by_id('hours_id').send_keys(str(self.segment_hours[i]))
+                driver.find_element_by_xpath('//*[@id="id____UID5"]/div/div/div').click()
 
-                wait.until(EC.element_to_be_clickable((By.ID, 'hours_id'))).send_keys(str(self.segment_hours[i]))
-                wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="id____UID5"]/div/div/div'))).click()
-                y += 1
+            # Submit Timesheet
+            driver.find_element_by_xpath('//*[@id="id____UID11"]/div/div/div').click()
 
-            # Submit Time Sheet For Approval
-            wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="id____UID11"]/div/div/div'))).click()
-
-        # Send Email
-        if banner_status != 0:
-            print('Sent Email!')
+            # Send Email
+            self.send_email(timesheet_period)
         else:
-            Automation.send_mail(self, self.time_period)
-        
-        # Browser Config
-        time.sleep(2)
+            print('Timesheet not submitted.')
+
+        # time.sleep(10000)
         driver.quit()
 
-        print(self.name)
-        print("--- %s seconds ---" % (time.time() - start_time))
-
-    def send_mail(self, time_period):
+    def send_email(self, timesheet_period):
         """Send's an email with user provided and sends it to our manager while also 
         sending it back to the sender for confirmation."""
 
         port = 587  # For starttls
         smtp_server = "smtp.office365.com"
         sender_email = f"{self.username}@louisiana.edu"
-        receiver_email = [credentials.RECIPIENT, sender_email]
+        receiver_email = [config["recipient"], sender_email]
         password = self.password
 
-        message = f'Subject: {self.name} Time Sheet Entry\n\n{time_period}\n{self.hours} Hours\n\n{self.name}\n{self.username}'
+        message = f'Subject: {self.name} Time Sheet Entry\n\n{timesheet_period}\n{self.hours_worked} Hours\n\n{self.name}\n{self.username}'
 
         server = smtplib.SMTP(smtp_server, port)
         server.starttls()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+
+start_time = time.time()
+
+try:
+    Auto('C00412627', 'Mandeville@2016', 58)
+except Exception as e:
+    print(e)
+
+finished_time = (time.time() - start_time)
+
+print(f'Finished in {round(finished_time, 2)} seconds.')
