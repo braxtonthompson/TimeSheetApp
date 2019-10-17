738acc38af044303b7f1bff7be6f6d897beefc1e
braxtontthompson <i3raxton@gmail.com>
2019-10-17 01:10:08 -0500

    Created config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+config = {
+    "recipient":"C00412627@louisiana.edu",
+    "secret_key": "c325bf048d4444685df73bfa95ce454c"
+}
+
+# Dana's Email
+# dana.jones@louisiana.edu
+
+# Braxton's Email
+# C00412627@louisiana.edu
+
+# Luke's Email
+# C00101509@louisiana.edu
+
+# Jacques' Info
+# C00078076@louisiana.edu
+# C00078076
+# Jpdub1597
